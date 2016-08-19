339bcbe5b34207afe11add33fae478045d17c0ef
rodrigo-frenk <furenku@gmail.com>
2016-08-18 21:55:41 -0500

    repo limpio

---
+++ src/html/functions.php
@@ -0,0 +1,3 @@
+<?php
+
+include_once 'funciones/cargar_recursos.php';
---
+++ src/html/single.php
@@ -0,0 +1,14 @@
+<?php
+get_header();
+
+if( have_posts() ) :
+   while ( have_posts() ) :
+      the_post();
+      ?>
+
+      <?php
+   endwhile;
+endif;
+
+get_footer();
+?>
