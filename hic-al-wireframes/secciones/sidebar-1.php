ab3de480ffda4be78e5ff1360b7735275e4a8556
josecaos <jazzvoon@gmail.com>
2016-10-12 23:58:13 -0500

    fixes sidebar titles

--- hic-al-wireframes/secciones/sidebar-1.php
+++ hic-al-wireframes/secciones/sidebar-1.php
@@ -1,34 +1,47 @@
 <section id="sidebar-1" class="columns medium-3 large-2 h-90-v text-center color-neutral_claro-bg p-t-1">
 
-  <h1 id="sidebar-1-widget-title" class="columns text-center h-5 font-sm-s color-black-bg top-right-corner white font-md-s font-lg-s bold uppercase">Temas</h1>
+  <!-- <h1 id="sidebar-1-widget-title" class="columns text-center h-5 font-sm-s color-primario-bg top-right-corner white font-md-s font-lg-s bold uppercase">Temas</h1> -->
+  <h1 id="sidebar-1-widget-title" class="columns text-center h-5 p-0 font-sm-s color-primario-bg top-right-corner white font-md-s font-lg-s bold uppercase">Temas</h1>
 
   <ul class="columns p-0 h-30">
-    <a href="#" class="h-10 v-center">
+    <a href="#" class="h-5 v-center font-sm-s font-md-s font-lg-s">
       <li class="columns h-a">sidebar menu</li>
     </a>
-    <a href="#" class="h-10 v-center">
+    <a href="#" class="h-5 v-center font-sm-s font-md-s font-lg-s">
       <li class="columns h-a">sidebar menu</li>
     </a>
-    <a href="#" class="h-10 v-center">
+    <a href="#" class="h-5 v-center font-sm-s font-md-s font-lg-s">
       <li class="columns h-a">sidebar menu</li>
     </a>
-    <a href="#" class="h-10 v-center">
+    <a href="#" class="h-5 v-center font-sm-s font-md-s font-lg-s">
       <li class="columns h-a">sidebar menu</li>
     </a>
-    <a href="#" class="h-10 v-center">
+    <a href="#" class="h-5 v-center font-sm-s font-md-s font-lg-s">
       <li class="columns h-a">sidebar menu</li>
     </a>
-    <a href="#" class="h-10 v-center">
+    <a href="#" class="h-5 v-center font-sm-s font-md-s font-lg-s">
       <li class="columns h-a">sidebar menu</li>
     </a>
-    <a href="#" class="h-10 v-center">
+    <a href="#" class="h-5 v-center font-sm-s font-md-s font-lg-s">
       <li class="columns h-a">sidebar menu</li>
     </a>
   </ul>
 
-  <p class="font-sm-s font-md-s font-lg-s h-30 p-2">Fuga nihil, enim vitae eum quasi hic repellendus esse, officia animi minima earum, eligendi consequuntur. Quidem, ratione nisi.</p>
-  <p class="font-sm-s font-md-s font-lg-s h-30 p-2">Fuga nihil, enim vitae eum quasi hic repellendus esse, officia animi minima earum, eligendi consequuntur. Quidem, ratione nisi.</p>
+  <div class="columns p-0 h-45">
+    <h1 id="sidebar-1-widget-title" class="columns text-center h-10 p-1 font-sm-s color-primario-bg top-right-corner white font-md-s font-lg-s bold uppercase">Temas</h1>
+    <div class="font-sm-s font-md-s font-lg-s p-2">
+
+      Molestiae aut consequatur unde harum doloribus a nisi delectus, architecto eius, id impedit, asperiores, illo assumenda. Doloribus, nam.
+    </div>
+  </div>
 
 
+  <div class="columns p-0 h-45">
+      <h1 id="sidebar-1-widget-title" class="columns text-center h-10 p-1 font-sm-s color-primario-bg top-right-corner white font-md-s font-lg-s bold uppercase">Temas</h1>
+      <div class="font-sm-s font-md-s font-lg-s p-2">
+
+        Molestiae aut consequatur unde harum doloribus a nisi delectus, architecto eius, id impedit, asperiores, illo assumenda. Doloribus, nam.
+      </div>
+    </div>
 
 </section>
--- hic-al-wireframes/secciones/sidebar-2.php
+++ hic-al-wireframes/secciones/sidebar-2.php
@@ -1,6 +1,20 @@
 <section id="sidebar-2" class="columns medium-3 large-2 h-90-v text-center color-neutral_claro-bg p-t-1">
 
-     <p class="font-sm-s font-md-s font-lg-s h-45 p-2">Fuga nihil, enim vitae eum quasi hic repellendus esse, officia animi minima earum, eligendi consequuntur. Quidem, ratione nisi.</p>
-     <p class="font-sm-s font-md-s font-lg-s h-45 p-2">Molestiae aut consequatur unde harum doloribus a nisi delectus, architecto eius, id impedit, asperiores, illo assumenda. Doloribus, nam.</p>
 
-</section>
+  <div class="columns p-0 h-45">
+    <h1 id="sidebar-1-widget-title" class="columns text-center h-10 p-1 font-sm-s color-primario-bg top-left-corner white font-md-s font-lg-s bold uppercase">Temas</h1>
+    <div class="font-sm-s font-md-s font-lg-s p-2">
+
+      Molestiae aut consequatur unde harum doloribus a nisi delectus, architecto eius, id impedit, asperiores, illo assumenda. Doloribus, nam.
+    </div>
+  </div>
+
+    <div class="columns p-0 h-45">
+      <h1 id="sidebar-1-widget-title" class="columns text-center h-10 p-1 font-sm-s color-primario-bg top-left-corner white font-md-s font-lg-s bold uppercase">Temas</h1>
+      <div class="font-sm-s font-md-s font-lg-s p-2">
+
+        Molestiae aut consequatur unde harum doloribus a nisi delectus, architecto eius, id impedit, asperiores, illo assumenda. Doloribus, nam.
+      </div>
+    </div>
+
+  </section>
