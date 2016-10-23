d9e5e94751d7f8a7015bec332281278999488220
josecaos <jazzvoon@gmail.com>
2016-10-23 13:59:54 -0500

    nuevos archivos de portafolio

---
+++ hic-al-wireframes/portfolio.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once "secciones/header.php";
+
+//
+include_once "secciones/sidebar-1.php";
+?>
+
+
+<!-- center-->
+<div id="central-container" class="columns medium-6 large-8 p-4">
+
+<?php
+
+include_once "secciones/portfolio/portfolio-grid.php";
+
+// include_once "secciones/portfolio/";
+
+// include_once "secciones/portfolio/";
+
+?>
+</div>
+<!-- center end -->
+
+
+<?php
+//
+include_once "secciones/sidebar-2.php";
+//
+
+include_once "secciones/footer.php";
+
+ ?>
---
+++ hic-al-wireframes/secciones/portfolio/portfolio-grid.php
@@ -0,0 +1,7 @@
+<section id="portfolio-grid" class="columns p-0 p-b-3">
+
+
+aqui el grid de el portafolio
+
+
+</section>
