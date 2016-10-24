e874db0c11ce892b810e95770fe2dbcac3842bc9
josecaos <jazzvoon@gmail.com>
2016-10-23 21:22:17 -0500

    interactividad portfolio parte 2 lista + fixes

--- hic-al-wireframes/js/app.js
+++ hic-al-wireframes/js/app.js
@@ -12,11 +12,10 @@ $(document).ready(function(){
 
   slider_latest_news();
 
-
-  // portforlio
+  slider_portfolio_preview();
   portfolio_preview();
 
-  slider_portfolio_preview();
+
   //
   setupResize();
 
@@ -60,33 +59,106 @@ function slider_portfolio_preview() {
     speed: 1000,
     autoplay:true,
     autoplaySpeed:5000,
-    slidesToShow:3
-    // slidesToScroll: 3
+    slidesToShow:3,
+    slidesToScroll:3
   });
 }
 
+
+
 function portfolio_preview() {
 
-  var preview = $('#portafolio-preview-canvas').detach();
+  var previewcanvas = $('#portafolio-preview-canvas');
+  var preview = $(previewcanvas).detach();
+  var antesid;
 
+  //
   $('.portfolio-item').each(function(index) {
 
 
-    $(this).on('click', function() {
+    $(this).on('click', function(e) {
+
+      var id = this.id;
+
+      // checa si esta abierto el preview
+      if ( $(previewcanvas).is(':visible') ) {
 
 
-        $(this).append(preview);
 
+        // si son iguales las ids anteriores solo cierra
+        if (id === antesid) {
+
+          // debug
+          console.log('ids iguales' + ' ' + id + ' ' + antesid);
+          $(previewcanvas).slideUp('fast');
+
+        } else {
+
+          // debug
+          console.log('ids desiguales' + ' ' + id + ' ' + antesid);
+
+          antesid = id;
+
+          $(previewcanvas).slideUp(250);
+          // incrusta preview
+          posiciona_preview($(this),preview);
+
+          // retrasa el desplegado del perview en la nueva posicion
+          setTimeout(function() {
+            $(previewcanvas).slideDown('fast');
+
+            cierra_preview(previewcanvas);
+
+          },300);
+          //
+        }
+        // endif desiguales
+
+      } else {
+
+        antesid = id;
+
+        // debug
+        console.log('no es visible' + ' ' + id + ' ' + antesid);
+
+        // incrusta preview
+        posiciona_preview($(this),preview);
+
+        // retrasa el desplegado del perview
         setTimeout(function() {
-          console.log("foto #" + index + preview);
 
-          $('#portafolio-preview-canvas').slideDown('fast');
+          $(previewcanvas).slideDown('fast');
+
+          cierra_preview(previewcanvas);
 
         },500);
+        //
 
+
+      }
+      // endif
 
     });
 
   });
+  //
+
+
+}
+
+function posiciona_preview(donde,pv) {
+  // de momento pega el preview despues del elemento clickeado
+  donde.after(pv);
+  //
+}
+
+function cierra_preview(pvc) {
+  // boton cerrar perview
+  $('#close-portfolio-preview').on('click', function() {
+
+    $(pvc).slideUp('fast');
+
+  });
+  //
 
 }
--- hic-al-wireframes/secciones/portfolio/portfolio-preview.php
+++ hic-al-wireframes/secciones/portfolio/portfolio-preview.php
@@ -1,7 +1,13 @@
 <section id="portafolio-preview-canvas" class="columns h-60-v color-gris-oscuro-bg color-blanco" style="display: none;">
 
+  <div id="close-portfolio-preview" class="columns p2 text-right h-10 pointer">
+
+    <i class="fa fa-remove bold font-xs-m font-sm-l font-md-xl font-lg-xxl color-blanco color-naranja-hover"></i>
+
+  </div>
+
   <!-- perview lado izquierdo imagen -->
-  <div id="portfolio-preview-img" class="columns small-12 medium-6 large-6 p-4">
+  <div id="portfolio-preview-img" class="columns small-12 medium-6 large-6 p-3 h-90">
 
     <div class="columns h-100 p-0 imgLiquid imgLiquidNoFill">
 
@@ -12,11 +18,19 @@
   </div>
 
   <!-- perview lado derecho texto + imagenes -->
-  <div id="portfolio-preview-texto" class="columns small-12 medium-6 large-6 p-4">
+  <div id="portfolio-preview-texto" class="columns small-12 medium-6 large-6 p-3 h-90">
 
     <div class="columns h-100 p-0">
 
-      <div class="columns h-50">lorem12</div>
+      <div class="columns h-50 p-0 ">
+
+        <div class="columns p-0 color-blanco h-a font-xs-m font-sm-m font-md-l font-lg-l">
+
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, earum praesentium nesciunt.
+
+        </div>
+
+      </div>
 
       <div id="portfolio-preview-thumbs" class="columns h-50 slick-portfolio-preview-thumbs">
 
@@ -25,16 +39,16 @@
 
         for ($i=0; $i < 8; $i++):
 
-          ?>
+        ?>
 
-          <div class="columns small-6 medium-4 large-3 h-100 p-l-0-2 p-r-0-2 imgLiquid imgLiquidNoFill">
+        <div class="columns h-100 p-l-0-2 p-r-0-2 imgLiquid imgLiquidNoFill">
 
-            <img src="http://fakeimg.pl/100?text=thumb" alt="">
+          <img src="http://fakeimg.pl/100?text=thumb" alt="">
 
-          </div>
+        </div>
 
 
-          <?php
+        <?php
         endfor;
 
         ?>
