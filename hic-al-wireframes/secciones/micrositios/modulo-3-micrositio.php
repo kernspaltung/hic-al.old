4a632dc3cbe418266fa7275fa81506f7aa221587
josecaos <jazzvoon@gmail.com>
2016-11-16 20:54:39 -0600

    WF micrositios modulo 4

---
+++ hic-al-wireframes/secciones/micrositios/modulo-3-micrositio.php
@@ -0,0 +1,76 @@
+<!-- DESTACADOS -->
+<section id="micrositio-modulo3" class="columns h-a">
+
+  <!-- elemento 3 -->
+
+  <?php
+
+  for ($i=0; $i < 2; $i++):
+
+    ?>
+
+
+
+    <article id="micrositio-modulo3-entrada" class="columns p-1 h-100-v">
+
+      <div class="columns p-0 h-30 imgLiquid imgLiquidFill">
+
+        <img src="http://fakeimg.pl/250?text=thumb" alt="">
+
+      </div>
+
+
+
+
+      <div class="columns h-70">
+
+        <!-- fecha -->
+        <div class="columns p-0 h-20 v-center">
+
+          <div class="columns p-0 h-a text-right font-xs-xs font-sm-s font-md-s font-lg-m">
+
+            16/11/2016
+
+          </div>
+
+        </div>
+
+        <!-- titulo -->
+        <div id="micrositio-portada-titulo" class="columns p-0 h-30 ">
+
+          <h1 class="columns h-1 p-0 text-left font-xs-xxl font-sm-xxl font-md-xxl font-lg-xxl">
+
+            Título completo de la entrada
+
+          </h1>
+
+        </div>
+
+
+        <!-- texto -->
+        <div class="columns p-0 h-30 v-center">
+
+          <div class="columns p-0 h-a font-xs-m font-sm-m font-md-l font-lg-l">
+
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio cupiditate velit culpa corporis excepturi accusamus dolore. Lorem ipsum dolor sit amet.
+
+          </div>
+
+        </div>
+
+
+      </div>
+
+    </article>
+    <!-- termina elemento -->
+
+
+    <?php
+
+  endfor;
+
+  ?>
+
+
+
+</section>
