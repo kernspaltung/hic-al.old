1c4814f3652112ca8a4ff551bd3bf7b1e8def2b5
josecaos <jazzvoon@gmail.com>
2016-11-16 19:20:35 -0600

    WF modulo 1 micrositios + heroscreen micrositios

--- hic-al-wireframes/micrositio.php
+++ hic-al-wireframes/micrositio.php
@@ -5,7 +5,11 @@ include_once "secciones/header-micrositio.php";
 
 
 
+//
+include_once "secciones/micrositios/portada-micrositio.php";
 
+//modulo 1
+include_once "secciones/micrositios/modulo-1-micrositio.php";
 
 
 
--- hic-al-wireframes/secciones/footer-micrositio.php
+++ hic-al-wireframes/secciones/footer-micrositio.php
@@ -16,7 +16,7 @@
 <footer id="footer-micrositio" class="header columns h-10-v color-gris-naranja-claro color-naranja-bg p-1">
 
   <div id="footer-micrositio-copyright" class="columns v-center">
-    <h2 class="columns p0 text-center font-xs-m font-sm-l font-md-xl font-lg-xl">
+    <h2 class="columns p0 text-center font-xs-l font-sm-l <font-md-x></font-md-x>l font-lg-xl">
       <i class="fa fa-copyright"></i> Hic-al 2016</h2>
     </div>
 
