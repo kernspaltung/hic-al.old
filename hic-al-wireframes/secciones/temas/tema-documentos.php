93e2a293c02e93bf0f5e5f626823efc46b725be6
josecaos <jazzvoon@gmail.com>
2016-11-16 23:03:04 -0600

    Plantilla temas 60% + merge

---
+++ hic-al-wireframes/secciones/temas/tema-documentos.php
@@ -0,0 +1,21 @@
+<section id="tema-documentos" class="columns small-12 medium-12 large-4 h-75-v show-for-large">
+
+
+documentos
+
+
+
+
+
+</section>
+
+<section id="tema-documentos" class="columns small-12 medium-12 large-4 h-40-v hide-for-large">
+
+
+documentos small medium
+
+
+
+
+
+</section>
