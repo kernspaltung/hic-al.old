54ecc1d32f86e62b0bbf24c50b17785d6ac404a7
josecaos <jazzvoon@gmail.com>
2016-11-17 21:19:29 -0600

    WF temas seccion noticias version movil listo

--- hic-al-wireframes/secciones/temas/tema-actividades.php
+++ hic-al-wireframes/secciones/temas/tema-actividades.php
@@ -9,13 +9,32 @@ actividades
 
 </section>
 
-<section id="tema-actividades" class="columns small-12 medium-12 large-4 h-40-v hide-for-large">
+<section id="tema-actividades" class="columns small-12 medium-12 large-4 h-60-v hide-for-large">
 
 
-actividades small medium
+  <!-- header seccion -->
+
+  <div class="columns p-0 h-a">
 
 
+    <div id="tema-actividades-titulo" class="columns small-6 h-20 p-1-3 font-xs-m font-sm-m font-md-m v-center">
+
+      Actividades
+
+    </div>
 
 
+    <a href="#" class="columns small-6 h-20">
+
+      <div id="tema-actividades-vermas" class="columns h-100 text-right p-1-3 font-xs-m font-md-m v-center">
+
+        Ver más <i class="fa fa-plus"></i>
+
+      </div>
+
+    </a>
+
+  </div>
+<!--  -->
 
 </section>
--- hic-al-wireframes/secciones/temas/tema-documentos.php
+++ hic-al-wireframes/secciones/temas/tema-documentos.php
@@ -9,13 +9,32 @@ documentos
 
 </section>
 
-<section id="tema-documentos" class="columns small-12 medium-12 large-4 h-40-v hide-for-large">
+<section id="tema-documentos" class="columns small-12 medium-12 large-4 h-60-v hide-for-large">
 
 
-documentos small medium
+  <!-- header seccion -->
+
+  <div class="columns p-0 h-a">
 
 
+    <div id="tema-documentos-titulo" class="columns small-6 h-20 p-1-3 font-xs-m font-sm-m font-md-m v-center">
+
+      Documentos
+
+    </div>
 
 
+    <a href="#" class="columns small-6 h-20">
+
+      <div id="tema-documentos-vermas" class="columns h-100 text-right p-1-3 font-xs-m font-md-m v-center">
+
+        Ver más <i class="fa fa-plus"></i>
+
+      </div>
+
+    </a>
+
+  </div>
+<!--  -->
 
 </section>
