ce0f7e5bbe072549b83619da082c943b093bc7dd
rodrigo-frenk <rodrigo.frenk@gmail.com>
2016-11-18 00:09:21 -0600

    home mejorado, movil y desktop

--- hic-al-wireframes/secciones/header.php
+++ hic-al-wireframes/secciones/header.php
@@ -2,229 +2,243 @@
 <html class="no-js" lang="en">
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>HIC-AL</title>
-  <link rel="stylesheet" href="bower_components/slick-carousel/slick/slick.css">
-  <link rel="stylesheet" href="bower_components/slick-carousel/slick/slick-theme.css">
-  <link rel="stylesheet" href="bower_components/leaflet/dist/leaflet.css">
+   <meta charset="utf-8">
+   <meta http-equiv="x-ua-compatible" content="ie=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>HIC-AL</title>
+   <link rel="stylesheet" href="bower_components/slick-carousel/slick/slick.css">
+   <link rel="stylesheet" href="bower_components/slick-carousel/slick/slick-theme.css">
+   <link rel="stylesheet" href="bower_components/leaflet/dist/leaflet.css">
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway">
 
-  <link rel="stylesheet" href="recursos/js_utils/dist/stylesheet/js_utils.min.css">
-  <link rel="stylesheet" href="css/app.css">
+   <link rel="stylesheet" href="recursos/js_utils/dist/stylesheet/js_utils.min.css">
+   <link rel="stylesheet" href="css/app.css">
 
 </head>
-<body>
+<body class="p-t-xs-3">
 
 
 
-    <section id="hero" class="columns h-70-sm-v h-md-80-v rel m-t-xs-3 m-t-md-0">
-      <div id="logo" class="columns small-6 medium-4 large-2 m-l-4 p-0 imgLiquid imgLiquidNoFill">
-        <img src="img/logo-300.png" alt="">
+   <section id="hero" class="columns h-70-v rel v-center rel">
+      <div id="imagen-header" class="columns h-80-v fixed top right z-1 p-0 imgLiquid imgLiquidFill">
+         <img src="https://junkitechture.files.wordpress.com/2015/04/2a5e2f23-aa20-4cc3-81bf-77fe701ff8cf-2060x1236.jpeg?w=1060" alt="">
       </div>
-
-      <div id="imagen-header" class="columns h-100 fixed top right z-1 p-0 imgLiquid imgLiquidFill">
-        <img src="http://www.hic-al.org/images/2015-06-29_18-35-10.jpg" alt="">
+      <div class="texto columns h-70-v absUpL z-0 v-center">
+         <div class="columns h-a">
+            <div id="logo" class="columns h-15-v h-md-100 medium-4 large-2 p-l-4 p-0 imgLiquid imgLiquidNoFill">
+               <img src="img/logo-300.png" alt="">
+            </div>
+            <div id="logo" class="columns h-sm-50 h-md-100 medium-8 large-10 p-l-4 p-0 v-center color-blanco">
+               <div class="columns h-a p-0">
+                  <h1 class=" txsh bold">
+                     Habitat International Coalition
+                  </h1>
+                  <h2 class=" txsh bold">
+                     América Latina
+                  </h2>
+               </div>
+            </div>
+         </div>
 
       </div>
-    </section>
+   </section>
 
-  <!-- header escritorio -->
-  <header id="header" class="header columns h-a show-for-large">
-
-
+   <!-- header escritorio -->
+   <header id="header" class="header columns h-a show-for-large">
 
 
-    <!-- menu desktop -->
-    <nav id="menu-desktop" class="columns medium-10 large-8 absUpR text-left h-a color-vino-bg bottom-left-corner" >
 
 
-      <!-- este se remueve con jquery -->
-      <div id="menu-list-container" class="columns h-menu-hical p-0">
-
-        <ul id="menu-list" class="columns medium-9 large-9 h-100 p-0 m-0">
-
-          <?php
-
-          $menu = array('Inicio', 'Mundo', 'Membresía','Calendario','Suscripción','Enlaces');
+      <!-- menu desktop -->
+      <nav id="menu-desktop" class="columns medium-10 large-8 absUpR text-left h-a color-vino-bg bottom-left-corner" >
 
 
-          for ($i=0; $i < 6 ; $i++):
+         <!-- este se remueve con jquery -->
+         <div id="menu-list-container" class="columns h-menu-hical p-0">
 
-            ?>
+            <ul id="menu-list" class="columns medium-9 large-9 h-100 p-0 m-0">
+
+               <?php
+
+               $menu = array('Inicio', 'Mundo', 'Membresía','Calendario','Suscripción','Enlaces');
 
 
-            <a href="#" class="shareW p-0 text-center color-blanco color-gris-naranja-claro-hover rel h-100 ">
+               for ($i=0; $i < 6 ; $i++):
 
-              <div class="columns p-0 h-100 v-center">
-
-                <li id="menu-element" class="columns p-0 text-center h-a borde-derecho font-md-s font-lg-m">
-
-                  <?php
-                  echo $menu[$i];
                   ?>
 
-                </li>
 
-              </div>
+                  <a href="#" class="shareW p-0 text-center color-blanco color-gris-naranja-claro-hover rel h-100 ">
 
-              <?php
+                     <div class="columns p-0 h-100 v-center">
 
-              if ($i == 0):
+                        <li id="menu-element" class="columns p-0 text-center h-a borde-derecho font-md-s font-lg-m">
 
-                ?>
-                <!-- senalador menu activo -->
-                <div id="active-pointer-<?php echo $i;?>" class="columns absDownL left h-5 menu-activo">
+                           <?php
+                           echo $menu[$i];
+                           ?>
 
-                  <div class="small-offset-5 menu-activo-triangulo">
+                        </li>
 
-                  </div>
+                     </div>
 
-                </div>
+                     <?php
 
+                     if ($i == 0):
 
+                        ?>
+                        <!-- senalador menu activo -->
+                        <div id="active-pointer-<?php echo $i;?>" class="columns absDownL left h-5 menu-activo">
 
+                           <div class="small-offset-5 menu-activo-triangulo">
 
+                           </div>
 
-                <?php
-              endif;
-
-              ?>
-
-
-
-
-            </a>
-
-            <?php
-          endfor;
-          ?>
-
-
-        </ul>
-
-        <div id="menu-element" class="columns medium-3 large-3 h-100 p-t-0-2 p-r-0">
-
-          <input type="text" class="columns p-0 h-80" placeholder="Search ...">
-
-        </div>
-
-      </div>
-
-
-    </nav>
-    <!-- end menu desktop -->
-
-
-  </header>
-  <!-- termina header escritorio -->
-
-
-  <!-- header movil -->
-  <header id="header-movil" class="columns h-10-v shadow header hide-for-large fixed top z1k1 left show-for-small p-0-2 show-form-medium color-white-bg">
-
-    <div id="logo-movil" class="columns small-3 medium-3 large-2 p-0 imgLiquid imgLiquidNoFill">
-      <img src="img/logo-50.png" alt="">
-    </div>
-
-
-    <div id="boton-menu-movil" class="columns small-2 medium-1 pointer fixed top right h-10-v v-center">
-
-      <i id="barras" class="fa fa-bars font-xs-l font-sm-xl font-md-xxl bold color-negro"></i>
-
-    </div>
+                        </div>
 
 
 
-    <!-- menu movil -->
-    <nav id="menu-movil" class="hidden columns medium-12 fixed top left top-menu-movil text-left h-75-v color-vino-bg" >
 
 
-      <div id="menu-list-container-movil" class="columns h-100 p-0">
+                        <?php
+                     endif;
 
-        <ul id="menu-list" class="columns medium-12 h-80 p-0 m-0">
-
-          <?php
-
-          $menu = array('Inicio', 'Mundo', 'Membresía','Calendario','Suscripción','Enlaces');
+                     ?>
 
 
-          for ($i=0; $i < 6 ; $i++):
-
-            ?>
 
 
-            <a href="#" class="columns p-0 text-center color-blanco borde-abajo color-gris-naranja-claro-hover h-15">
-
-              <div class="columns p-0 p-t-1">
-
-                <li id="menu-element" class="columns vcenter text-center h-a">
+                  </a>
 
                   <?php
-                  echo $menu[$i];
-                  ?>
-
-                </li>
-
-              </div>
-
-              <?php
-
-              if ($i == 0):
-
-                ?>
+               endfor;
+               ?>
 
 
+            </ul>
+
+            <div id="menu-element" class="columns medium-3 large-3 h-100 p-t-0-2 p-r-0">
+
+               <input type="text" class="columns p-0 h-80" placeholder="Search ...">
+
+            </div>
+
+         </div>
 
 
-                <?php
-              endif;
-
-              ?>
+      </nav>
+      <!-- end menu desktop -->
 
 
+   </header>
+   <!-- termina header escritorio -->
 
 
-            </a>
+   <!-- header movil -->
+   <header id="header-movil" class="columns h-10-v shadow header hide-for-large fixed top z1k1 left show-for-small p-0-2 show-form-medium color-white-bg">
+
+      <div id="logo-movil" class="columns small-3 medium-3 large-2 p-0 imgLiquid imgLiquidNoFill">
+         <img src="img/logo-50.png" alt="">
+      </div>
 
 
-            <?php
-          endfor;
-          ?>
+      <div id="boton-menu-movil" class="columns small-2 medium-1 pointer fixed top right h-10-v v-center">
 
-
-        </ul>
-
-
-
-        <div id="menu-element" class="columns medium-12 h-20 p-t-0-2 p-r-0 p-l-0">
-
-          <input type="text" class="columns p-0 h-80" placeholder="Buscar ...">
-
-        </div>
+         <i id="barras" class="fa fa-bars font-xs-l font-sm-xl font-md-xxl bold color-negro"></i>
 
       </div>
 
 
-    </nav>
-    <!-- end menu movil -->
+
+      <!-- menu movil -->
+      <nav id="menu-movil" class="hidden columns medium-12 fixed top left top-menu-movil text-left h-75-v color-vino-bg" >
 
 
-  </header>
-  <!-- termina header movil -->
+         <div id="menu-list-container-movil" class="columns h-100 p-0">
 
-  <?php
-include_once "secciones/menu-scroll.php";
+            <ul id="menu-list" class="columns medium-12 h-80 p-0 m-0">
 
+               <?php
 
-?>
-
-<div id="main-container" class="columns w-100-v h-a m-0 p-0 color-white-bg">
+               $menu = array('Inicio', 'Mundo', 'Membresía','Calendario','Suscripción','Enlaces');
 
 
-<?php
+               for ($i=0; $i < 6 ; $i++):
 
-include_once "secciones/sidebar-1.php";
-?>
+                  ?>
 
-  <main id="main" class="columns large-8 p-0 h-a">
+
+                  <a href="#" class="columns p-0 text-center color-blanco borde-abajo color-gris-naranja-claro-hover h-15">
+
+                     <div class="columns p-0 p-t-1">
+
+                        <li id="menu-element" class="columns vcenter text-center h-a">
+
+                           <?php
+                           echo $menu[$i];
+                           ?>
+
+                        </li>
+
+                     </div>
+
+                     <?php
+
+                     if ($i == 0):
+
+                        ?>
+
+
+
+
+                        <?php
+                     endif;
+
+                     ?>
+
+
+
+
+                  </a>
+
+
+                  <?php
+               endfor;
+               ?>
+
+
+            </ul>
+
+
+
+            <div id="menu-element" class="columns medium-12 h-20 p-t-0-2 p-r-0 p-l-0">
+
+               <input type="text" class="columns p-0 h-80" placeholder="Buscar ...">
+
+            </div>
+
+         </div>
+
+
+      </nav>
+      <!-- end menu movil -->
+
+
+   </header>
+   <!-- termina header movil -->
+
+   <?php
+   include_once "secciones/menu-scroll.php";
+
+
+   ?>
+
+   <div id="main-container" class="columns w-100-v h-a m-0 p-0 color-white-bg">
+
+
+      <?php
+
+      include_once "secciones/sidebar-1.php";
+      ?>
+
+      <main id="main" class="columns large-8 p-0 h-a">
--- hic-al-wireframes/secciones/sidebar-1.php
+++ hic-al-wireframes/secciones/sidebar-1.php
@@ -1,98 +1,105 @@
-<section id="sidebar-1" class="columns medium-6 large-2 h-100-v text-center p-t-1 p-l-0" data-sticky-container>
+<section id="sidebar-1" class="columns medium-6 large-2 h-100-v text-center m-t-1 p-l-0" data-sticky-container>
 
-<!-- sticky -->
+   <!-- sticky -->
 
-<div class="columns p-0 sticky" data-top-anchor="main:top" data-btm-anchor="main:bottom" data-sticky-on="large" data-sticky>
-
-
-
-  <div class="columns p-0 h-a m-b-1">
-
-    <div class="columns p-0 h-a">
-      <h6 id="sidebar-1-widget-title" class="columns text-center font-sm-s color-blanco color-vino-oscuro-bg p-0-2 h-a vcenter top-right-corner white font-md-s font-lg-s bold uppercase">Temas</h6>
-    </div>
-
-    <ul class="columns p-0 h-30">
-
-      <?php
-      for ($i=0; $i < 6; $i++):
+   <div class="columns p-0 sticky h-100-v" data-top-anchor="main:top" data-btm-anchor="main:bottom" data-sticky-on="large" data-sticky data-margin-top="0">
 
 
-        ?>
+
+      <div class="columns h-65 p-0 m-0">
+
+         <!-- <div class="columns p-0 h-a">
+            <h6 id="sidebar-1-widget-1-title" class="columns text-center font-sm-s color-blanco color-vino-oscuro-bg p-0-2 h-a vcenter top-right-corner white font-md-s font-lg-s bold uppercase">Temas</h6>
+         </div> -->
+
+         <div class="columns h-100 p-0 ">
+
+            <?php
+
+$colors_bg = [ 'color-vino-claro-bg', 'color-vino-intenso-bg', 'color-vino-medio-bg', 'color-vino-oscuro-bg', 'color-vino-bg' ];
+            for ($i=0; $i < 8; $i++):
 
 
-        <a href="#" class="shareH font-sm-s font-md-s font-lg-s">
-          <li class="columns h-a p-0-1 color-blanco color-vino-bg">sidebar menu</li>
-        </a>
+               ?>
 
 
-        <?php
 
-      endfor;
-
-      ?>
-
-
-    </ul>
-  </div>
-
-
-  <!--  -->
-  <div class="columns p-0">
-
-    <div class="columns p-0 h-a">
-      <h6 id="sidebar-1-widget-title" class="columns text-center font-sm-s color-blanco color-gris-oscuro-bg p-0-2 h-a vcenter top-right-corner white font-md-s font-lg-s bold uppercase">Medios</h6>
-    </div>
-
-    <div class="font-sm-s font-md-s font-lg-s p-0 h-50">
-
-      <?php
-      // 7 numero en la referencia
-      for ($i=0; $i < 4 ; $i++):
-
-        ?>
+               <div class="columns h-a shareH p-0">
+                  <div class="columns font-xs text-center card color-blanco text-left v-center <?php echo $colors_bg[rand(0,4)]; echo $i==0 ? ' top-right-corner' : ''; ?>">
+                     <div class="columns h-a p-0">
+                        Megaproyectos y Alternativas
+                     </div>
+                  </div>
+               </div>
 
 
-        <div class="columns medium-12 p-0 shareH ">
+               <?php
 
-          <div class="columns h-30 p-0 ">
+            endfor;
 
-            <h6 class="columns h-a vcenter p-0 font-sm-s font-md-s font-lg-s">
+            ?>
 
-              Titulo
 
+         </div>
+      </div>
+
+
+      <!--  -->
+      <div class="columns h-35 p-0">
+
+         <!-- <div class="columns p-0 h-a">
+            <h6 id="sidebar-1-widget-2-title" class="columns text-center font-sm-s color-blanco color-gris-oscuro-bg p-0-2 h-a vcenter top-right-corner white font-md-s font-lg-s bold uppercase">
+               Medios
             </h6>
+         </div> -->
 
-          </div>
+         <div class="columns font-s p-0 ">
 
-          <div class="columns h-70 p-0 imgLiquid imgLiquidFill">
+            <?php
+            // 7 numero en la referencia
+            $colors_bg = [ 'color-verde-claro-intenso', 'color-vino-intenso', 'color-naranja', 'color-vino-oscuro', 'color-verde' ];
+            for ($i=0; $i < 8 ; $i++):
 
-            <img src="http://fakeimg.pl/300?text=medios" alt="">
-
-          </div>
-
-        </div>
-
-
-
-        <?php
-
-      endfor;
-
-      ?>
+               ?>
 
 
-    </div>
-
-  </div>
 
 
-  <!-- <div class="columns p-0 h-30">
-  <h1 id="sidebar-1-widget-title" class="columns text-center h-5 p-1 font-sm-s color-primario-bg top-right-corner white font-md-s font-lg-s bold uppercase">Temas</h1>
-  <div class="font-sm-s font-md-s font-lg-s p-2">
 
-  Molestiae aut consequatur unde harum doloribus a nisi delectus, architecto eius, id impedit, asperiores, illo assumenda. Doloribus, nam.
-</div>
+               <div class="columns small-6 h-25 p-0">
+
+
+                  <div class="columns p-0 v-center card color-gris-claro-bg">
+                     <div class="columns h-a p-0 font-xxs">
+                        <i class="fa fa-camera-retro font-m <?php echo $colors_bg[rand(0,4)]; ?>"></i>
+                     </div>
+                     <div class="columns h-a p-0 font-xxs bold">
+                        Publicaciones
+                     </div>
+                  </div>
+
+               </div>
+
+
+
+               <?php
+
+            endfor;
+
+            ?>
+
+
+         </div>
+
+      </div>
+
+
+      <!-- <div class="columns p-0 h-30">
+      <h1 id="sidebar-1-widget-title" class="columns text-center h-5 p-1 font-sm-s color-primario-bg top-right-corner white font-md-s font-lg-s bold uppercase">Temas</h1>
+      <div class="font-sm-s font-md-s font-lg-s p-2">
+
+      Molestiae aut consequatur unde harum doloribus a nisi delectus, architecto eius, id impedit, asperiores, illo assumenda. Doloribus, nam.
+   </div>
 </div> -->
 
 
